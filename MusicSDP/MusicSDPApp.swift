4009cd5e83e24bccc8089ea30e82d2bcdf42fcdc
Ignacio Hernaiz <nacho_celtic@hotmail.com>
2025-12-08 21:09:48 +0100

    Initial Commit

---
+++ MusicSDP/MusicSDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSDPApp.swift
+//  MusicSDP
+//
+//  Created by IGNACIO HERNAIZ IZQUIERDO on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicSDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
